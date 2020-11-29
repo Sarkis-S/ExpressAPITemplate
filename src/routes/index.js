ae10cb9788c43ff4b46ebb8680bf1e489cc85bc8
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-11-29 14:32:52 -0800

    Create first test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../config';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Express API Template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
